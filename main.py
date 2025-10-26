a7954bd9262ce5226276f40d175aa1752d3ad6e7
Tinashe Kucherera <tkucherera86@gmail.com>
2025-10-26 19:14:49 -0400

    init data

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Study of Housing Market Trends in Harare, Zimbabwe
+
+Data:
+  - Property Prices 
+  - Rental Rates 
+  - Population 
+  - Inflation Rates 
+  - Remmittances
+  - Number of Properties 
+
+"""
+
